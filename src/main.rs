60db6068f28ddbc112ab5d9d73f34fe566370282
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-23 22:14:49 -0400

    feat: created fetch joke function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
+use reqwest::Response;
 use yew::prelude::*;
+use serde_json::Value;
+
+#[derive(Clone, Debug, PartialEq)]
+struct Joke {
+    value: String,
+}
+
+async fn fetch_joke() -> Joke {
+    const URL: &str = "https://api.yomomma.info";
+
+    let response: Response = reqwest::get(URL).await.unwrap();
+
+    let text: String = response.text().await.unwrap();
+
+    let serialized_response: Value = serde_json::from_str(&text).unwrap();
+
+    let joke: &str = serialized_response["joke"].as_str().unwrap();
+
+    return Joke {
+        value: String::from(joke),
+    };
+}
 
 #[function_component(App)]
 fn app() -> Html {
