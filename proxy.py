baf1191cb4165aa824da2092f461c7b3283227ff
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-24 13:25:44 -0400

    feat: created proxy for client fetching api without cors issues

---
+++ proxy.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
+import requests
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+load_dotenv()
+
+
+@app.route('/')
+def home():
+    return "Hello!!"
+
+
+@app.route('/jokes', methods=['GET'])
+@cross_origin(supports_credentials=True)
+def get_jokes():
+    response = requests.get('https://api.yomomma.info')
+
+    return response.json()
+
+
+app.run()
